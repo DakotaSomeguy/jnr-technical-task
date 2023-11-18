37b7dcf5549e0f9b9cf958af93e296cf343bc4b0
Dakota Walton <DakotaDWalton@outlook.com>
2023-11-18 14:01:56 +1000

    Render multiple cards with different data

--- src/App.js
+++ src/App.js
@@ -6,13 +6,34 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Token from "./components/Token";
 
-
+const tokenData = [
+  {
+    index: "#1",
+    ticker: "BTC",
+    sharesTraded: "510.21 Bn",
+    priceTraded: "$26,123.21",
+    changeDirectionUp: true,
+    change: "1.50%",
+  },
+  {
+    index: "#2",
+    ticker: "BCH",
+    sharesTraded: "510.21 Bn",
+    priceTraded: "$26,123.21",
+    changeDirectionUp: false,
+    change: "1.50%",
+  },
+];
 
 function App() {
   return (
     <div className="">
       <Header />
-      <Token index={"#1"} ticker={"BTC"} sharesTraded={"510.21 Bn"} priceTraded={"$26,123.21"} change={"1.50%"}/>
+      {
+        tokenData.map((token) => (
+            <Token tokenData={token} />
+        ))
+      }
       <Footer />
     </div>
   );
--- src/components/Token.jsx
+++ src/components/Token.jsx
@@ -1,9 +1,9 @@
-export default function Token({index, icon, ticker, sharesTraded, priceTraded, change }) {
+export default function Token({tokenData }) {
     return (
         <div className="w-96 h-14 relative">
         <div className="w-96 h-14 left-0 top-0 absolute bg-zinc-800 rounded" />
         <div className="left-[24px] top-[21px] absolute text-white text-xs font-medium font-['Inter']">
-          {index}
+          {tokenData.index}
         </div>
         <img
           className="w-7 h-7 left-[61px] top-[14px] absolute rounded-full"
@@ -11,23 +11,32 @@ export default function Token({index, icon, ticker, sharesTraded, priceTraded, c
         />
         <div className="left-[101px] top-[12px] absolute w-20">
           <div className="left-0 top-0 absolute text-white text-sm font-bold font-['Inter']">
-            {ticker}
+            {tokenData.ticker}
           </div>
           <div className="left-0 top-[19px] absolute text-neutral-500 text-xs font-normal font-['Inter']">
-            {sharesTraded}
+            {tokenData.sharesTraded}
           </div>
         </div>
         <div className="left-[200px] top-[21px] absolute text-right text-white text-xs font-bold font-['Inter']">
-          {priceTraded}
+          {tokenData.priceTraded}
         </div>
     
-        <div className="p-0.5 left-[296px] top-[19px] absolute bg-green-500 bg-opacity-10 rounded flex-col justify-start items-start gap-2.5 inline-flex">
+        {tokenData.changeDirectionUp ? 
+        (<div className="p-0.5 left-[296px] top-[19px] absolute bg-green-500 bg-opacity-10 rounded flex-col justify-start items-start gap-2.5 inline-flex">
           <div className="relative">
             <div className="left-[10px] top-0 absolute text-right text-green-500 text-xs font-bold font-['Inter']">
-              {change}
+              {tokenData.change}
             </div>
           </div>
-        </div>
+        </div>): (
+            <div className="p-0.5 left-[296px] top-[19px] absolute bg-red-500 bg-opacity-10 rounded flex-col justify-start items-start gap-2.5 inline-flex">
+            <div className="relative">
+              <div className="left-[10px] top-0 absolute text-right text-red-500 text-xs font-bold font-['Inter']">
+                {tokenData.change}
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     )
 }
