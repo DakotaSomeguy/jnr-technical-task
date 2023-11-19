1a1f3114d30d61b735965cbebf8a1df3aef69f75
Dakota Walton <DakotaDWalton@outlook.com>
2023-11-19 19:21:10 +1000

    Display My Token data from MongoDB

--- src/components/MyTokens.js
+++ src/components/MyTokens.js
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Token from "./Token";
-import useTokenStore from "./tokenStore"; // Adjust the path accordingly
 
 const MyTokens = () => {
-  const { selectedTokens } = useTokenStore();
+  const [myTokens, setMyTokens] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Fetch tokens from the server
+    fetch('http://127.0.0.1:3001/api/my-tokens')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((tokens) => {
+        setMyTokens(tokens);
+      })
+      .catch((error) => {
+        console.error('Error fetching tokens:', error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
 
   return (
-    <div className="grid justify-items-center" >
+    <div className="grid justify-items-center">
       <h1 className="text-3xl font-bold mt-10">My Tokens Page</h1>
       <div className="h-56 grid grid-cols-3 gap-4 content-start m-10">
-        {selectedTokens.map((token) => (
-          <Token
-            key={token.index}
-            tokenData={token}
-          />
-        ))}
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          myTokens.map((token) => (
+            <Token key={token.index} tokenData={token} />
+          ))
+        )}
       </div>
     </div>
   );
